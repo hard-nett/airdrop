21088e3f765b4ac000ccfebc6fcd4a9040bd8c0e
hard-nett <hardnettt@proton.me>
2025-12-23 23:23:24 -0500

    remove commonware runtime fromzk-headstash

--- zk-crates/zk-headstash/src/bin/create_headstash_proof.rs
+++ zk-crates/zk-headstash/src/bin/create_headstash_proof.rs
@@ -1,5 +1,5 @@
-use commonware_runtime::tokio::Runner;
-use commonware_runtime::Runner as _;
+// use commonware_runtime::tokio::Runner;
+// use commonware_runtime::Runner as _;
 use zk_headstash::address::RecpAddr;
 use zk_headstash::deploy::suite::*;
 use zk_headstash::keys::EligibleSk;
@@ -13,18 +13,18 @@ use zk_headstash::Anchor;
 /// ```
 /// See <https://docs.rs/commonware_runtime> for traits/details.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Runner::default()
-        .start(|_| async move {
-            let a = Anchor::empty_tree();
-            let mp =
-                MerklePath::from_parts(0, [MerkleHashOrchard::from_bytes(&[69; 32]).unwrap(); 32]);
-            let esk = EligibleSk::from_bytes([42u8; 32]);
-            let recp = RecpAddr::new([42; 32]);
-            let hv = HeadstashValue::new(100.into(), NoteDenom::new_for_proof("uterp"), 6);
-            HeadstashSuite::new()
-                .create_headstash_proof(a, mp, esk, recp, hv)
-                .await
-        })
-        .map_err(|e| e as Box<dyn std::error::Error>)?;
+    // Runner::default()
+    //     .start(|_| async move {
+    //         let a = Anchor::empty_tree();
+    //         let mp =
+    //             MerklePath::from_parts(0, [MerkleHashOrchard::from_bytes(&[69; 32]).unwrap(); 32]);
+    //         let esk = EligibleSk::from_bytes([42u8; 32]);
+    //         let recp = RecpAddr::new([42; 32]);
+    //         let hv = HeadstashValue::new(100.into(), NoteDenom::new_for_proof("uterp"), 6);
+    //         HeadstashSuite::new()
+    //             .create_headstash_proof(a, mp, esk, recp, hv)
+    //             .await
+    //     })
+    //     .map_err(|e| e as Box<dyn std::error::Error>)?;
     Ok(())
 }
