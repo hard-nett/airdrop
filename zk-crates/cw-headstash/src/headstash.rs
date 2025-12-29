ec405ac50883474a43fd9b0952a7666fef5ae835
hard-nett <hardnettt@proton.me>
2025-12-28 20:38:36 -0500

    zk-headstash: verify proof

--- zk-crates/cw-headstash/src/headstash.rs
+++ zk-crates/cw-headstash/src/headstash.rs
@@ -132,7 +132,10 @@ pub fn process_headstash(
         }
 
         // verify headstash proof
-        // Proof::new(claim.p.to_vec()).verify(&VK, &[claim.i.clone().into()])?;
+        deps.api.halo2_proof_instance_verify(
+            &claim.p.to_vec(),
+            &<HeadstashInstances as Into<Instance>>::into(claim.i.clone()).to_bytes(),
+        )?;
 
         // verify recp integrity
         claim.verify_recp_posiedon_hash()?;
@@ -189,7 +192,6 @@ pub fn process_headstash(
             }
         }
     }
-    // }
 
     Ok(res.add_attribute("action", "process_headstash"))
 }
